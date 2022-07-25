79b84cb510e15755fdb2c83008f74b1cbec4fb8a
Tounani Abderrahmane <74843162+ET-TOUNANI@users.noreply.github.com>
2022-07-25 17:56:48 +0100

    Create GetButtonNavigatBar.config.dart

---
+++ lib/configs/GetButtonNavigatBar.config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+GetButtonNavigatBar(context){
+
+  return BottomNavigationBar(
+    currentIndex: 1,
+    onTap: (index)  {
+      if(index==0){
+        Navigator.pushNamed(context, "/");
+      }
+      else{
+
+      }
+    },
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: Color(0xff045448),
+    selectedItemColor: Color(0xffFFE29D),
+    unselectedItemColor: Color(0xffFFE29D),
+    items: const [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: "الرئيسية",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(
+            Icons.help),
+        label: "حول",
+      ),
+    ],
+  );
+}
